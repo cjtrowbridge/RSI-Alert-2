1d4ed481199b09820187290f93fd88afaf94c606
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2021-05-21 01:12:23 -0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,12 @@
 <?php
 
+$Filename = 'cache/'.date('Y-m-d').'.json';
+
+if(!(file_exists($Filename))){
+  //Need to fetch current quote.
+  include('Config.php');
+  $URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY='.$Key;
+  $Data = file_get_contents($URL);
+  file_put_contents($Filename,$Data);
+}
+
