cfee58e5d703211ae68eae45e2b7c16833ad8e90
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2021-05-21 01:20:30 -0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -10,6 +10,8 @@ if(!(file_exists($Filename))){
   file_put_contents($Filename,$Data);
 }
 
+$Coins = array();
+
 //Check if we have the last 14 days of data
 for ($i = 0; $i <= 14; $i++) {
   $Date = time() - (60*60*24*$i)  ;
@@ -17,8 +19,19 @@ for ($i = 0; $i <= 14; $i++) {
   if(file_exists($Filename)){
     $Data = file_get_contents($Filename);
     $JSON = json_decode($Data);
-    var_dump($JSON);
+    foreach($JSON['data'] as $Coin){
+      $Name = $Coin['name'];
+      if(
+        (!(isset($Coins[$Name]))) ||
+        (!(is_array($Coins[$Name])))
+      )){
+        $Coins[$Name] = array();
+      }
+      
+    }
   }else{
     die('Data Missing For Date: '.date('Y-m-d',$Date));
   }
 }
+
+var_dump($Coins);
