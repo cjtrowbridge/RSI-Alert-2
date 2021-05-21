06eec78aef69b58f59e9deee68d415ad2063c09b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2021-05-21 13:31:46 -0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -147,6 +147,8 @@ foreach($Coins as $Coin){
   //If we couldn't some data then prompt the user to input it.
   if(!(empty($Missing))){
     echo '<p><a href="./?action=enterMissing">Click here</a> to enter missing data for '.$Symbol.'.</p>';
+  }else{
+    pd($Missing);
   }
   pd($RS[$Symbol]);
   //Done with this coin
